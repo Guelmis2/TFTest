4143f2fcf7589c7c8526aa6e0294bad21476335d
Guelmis Edwards <guelmisedwards@gmail.com>
2023-08-23 17:21:13 -0400

    first commit

---
+++ first_ec2.tf
@@ -0,0 +1,48 @@
+provider "aws" {
+#   region     = "us-east-2"
+#   access_key = "AKIAVI233UEHNWWOZWDJ"
+#   secret_key = "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"
+}
+
+resource "aws_instance" "gman_ec2" {
+
+  ami           = "ami-0ccabb5f82d4c9af5"
+  instance_type = "t2.micro"
+  tags =  {
+    Name = "F23MAN"
+  }
+
+}
+
+# resource "aws_instance" "gman2_ec2" {
+
+#   ami           = "ami-0ccabb5f82d4c9af5"
+#   instance_type = "t2.micro"
+#   tags =  {
+#     Name = "F32MAN"
+#   }
+
+# }
+
+# data "aws_ami" "ami" {
+#   most_recent = true
+
+#   filter {
+#     name   = "name"
+#     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+#   }
+
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
+
+#   owners = ["099720109477"] # Canonical
+# }
+
+
+
+#   tags = {
+#     Name = "HelloWorld"
+#   }
+# }
